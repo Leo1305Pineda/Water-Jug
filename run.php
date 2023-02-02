9eb57968f7f4b611ac807f0e451a4d42f9a66f31
LeonardoPineda <leo135pineda@gmail.com>
2023-02-01 20:18:10 -0400

    updated run.php

--- run.php
+++ run.php
@@ -1,49 +1,78 @@
 <?php
-// Define the capacities of the two Water Jug
-$water_jug1 = 2;
-$water_jug2 = 10;// Define the target volume of water
-$target = 4;// Create a queue to store the states of the jugs
-$queue = array(); // Create a visited array to keep track of the states we've already seen
-$visited = array(); // Push the initial state onto the queue
-array_push($queue, array(0, 0)); // Keep looping until the queue is empty
-while (!empty($queue)) {
- // Pop the next state off the queue
- $state = array_shift($queue); // If the current state is the target, we're done!
- if ($state[0] == $target || $state[1] == $target) {
- echo "Found solution: (" . implode(", ", $state) . ")\n";
- break;
- } // If we haven't seen this state before, add it to the visited array
- if (!in_array($state, $visited)) {
- array_push($visited, $state); // Generate all possible next states by pouring water from one jug to another
- // Pour from jug 1 to jug 2
- $nextState = array($state[0], $state[0] + $state[1]);
- if ($nextState[0] > $water_jug1) {
- $nextState[0] = $water_jug1;
- $nextState[1] = $state[0] + $state[1] - $water_jug1;
- }
- if ($nextState[1] > $water_jug1) {
- $nextState[1] = $water_jug1;
- $nextState[0] = $state[0] - ($water_jug1 - $state[1]);
- }
- array_push($queue, $nextState); // Pour from jug 2 to jug 1
- $nextState = array($state[0] + $state[1], $state[1]);
- if ($nextState[0] > $water_jug1) {
- $nextState[0] = $water_jug1;
- $nextState[1] = $state[0] + $state[1] - $water_jug1;
- }
- if ($nextState[1] > $water_jug1) {
- $nextState[1] = $water_jug1;
- $nextState[0] = $state[0] - ($water_jug2 - $state[1]);
- }
- array_push($queue, $nextState); // Fill jug 1
- $nextState = array($water_jug1, $state[1]);
- array_push($queue, $nextState); // Fill jug 2
- $nextState = array($state[0], $water_jug2);
- array_push($queue, $nextState); // Empty jug 1
- $nextState = array(0, $state[1]);
- array_push($queue, $nextState); // Empty jug 2
- $nextState = array($state[0], 0);
- array_push($queue, $nextState);
- }
+function _fill($state, $water_jug, $type = 1)
+{
+	if ($type) {
+		return array($water_jug, $state[$type]);
+	}
+	return array($state[$type], $water_jug);
 }
-?>
+
+function _empty($state, $type = 1)
+{
+	if ($type) {
+		return array(0, $state[$type]);
+	}
+	return array($state[$type], 0);
+}
+
+function _transfer($state, $water_jug1, $water_jug2, $type = 1)
+{
+	if ($type == 1) {
+		$nextState = array($state[0], $state[0] + $state[1]);
+		if ($nextState[0] > $water_jug1) {
+			$nextState[0] = $water_jug1;
+			$nextState[1] = $state[0] + $state[1] - $water_jug1;
+		}
+		if ($nextState[1] > $water_jug1) {
+			$nextState[1] = $water_jug1;
+			$nextState[0] = $state[0] - ($water_jug1 - $state[1]);
+		}
+		return $nextState;
+	}
+
+	$nextState = array($state[0] + $state[1], $state[1]);
+	if ($nextState[0] > $water_jug1) {
+		$nextState[0] = $water_jug1;
+		$nextState[1] = $state[0] + $state[1] - $water_jug1;
+	}
+	if ($nextState[1] > $water_jug1) {
+		$nextState[1] = $water_jug1;
+		$nextState[0] = $state[0] - ($water_jug2 - $state[1]);
+	}
+
+	return $nextState;
+}
+
+function process($water_jug1, $water_jug2, $target)
+{
+	$queue = array(); // Create a visited array to keep track of the states we've already seen
+	$visited = array(); // Push the initial state onto the queue
+	array_push($queue, array(0, 0)); // Keep looping until the queue is empty
+	$result = 'No Solution';
+	while (!empty($queue)) {
+		// Pop the next state off the queue
+		$state = array_shift($queue); // If the current state is the target, we're done!
+		if ($state[0] == $target || $state[1] == $target) {
+			$result = "Found solution: (" . implode(", ", $state) . ")\n";
+			break;
+		} // If we haven't seen this state before, add it to the visited array
+		if (!in_array($state, $visited)) {
+			array_push($visited, $state); // Generate all possible next states by pouring water from one jug to another
+
+			array_push($queue, _transfer($state, $water_jug1, $water_jug2)); // Pour from jug 2 to jug 1
+			array_push($queue, _transfer($state, $water_jug1, $water_jug2, 2)); // Pour from jug 1 to jug 2
+
+			array_push($queue, _fill($state, $water_jug1)); // Fill jug 1
+			array_push($queue, _fill($state, $water_jug2, 0)); // Fill jug 2
+			
+			array_push($queue, _empty($state)); // Empty jug 1
+			array_push($queue, _empty($state, 0)); // Empty jug 2
+		}
+	}
+	return $result;
+}
+
+// Define the capacities of the two Water Jug
+// Define the target volume of water
+// Create a queue to store the states of the jugs
+echo process(2, 10, 4);
